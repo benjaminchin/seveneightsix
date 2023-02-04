4d5190ea6fbd4697d442bba894633d4e41c944d7
JakeJewsbury <jake.jewsbury@gmail.com>
2023-02-04 18:48:41 -0500

    Finished algorithm

--- src/Testing2.py
+++ src/Testing2.py
@@ -1,5 +1,17 @@
 import requests
-response = requests.get('https://ecofriendlyweb.org/eco-friendly-website-accreditation/')
-x = len(response.content)
-print(x)
-print('word')
+
+link = "https://observablehq.com/@mrchrisadams/carbon-footprint-of-sending-data-around"
+
+#Have field where variable called "link" = user input, and  
+response = requests.get(link) 
+size = len(response.content)
+
+KB = int(size)/1024
+MB = KB/1024
+
+#Have users enter the amount of monthly visitors = variable "visitors"
+visitors = 10000
+#Equation for Total data used by website and users
+netdata = MB*visitors
+emissions = 0.00086638382*netdata
+print('The monthly emissions associated with your website amounts to ' + str(emissions) + ' kilograms of CO2')
