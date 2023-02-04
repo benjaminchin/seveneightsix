cafe1e5eaac8a4febfbcdc96030574c495c54a54
benjaminchin <benjaminchin@vt.edu>
2023-02-04 16:43:37 -0500

    main work

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
 from flask import Flask, request, render_template
+from browsermobproxy import Server
+from selenium import webdriver
+
+server = Server(
+    "eco-website/venv/Lib/site-packages/browsermob-proxy-2.1.4-bin/browsermob-proxy-2.1.4/bin/")
+server.start()
+proxy = server.create_proxy()
+
+profile = webdriver.FirefoxProfile()
+profile.set_proxy(proxy.selenium_proxy())
+driver = webdriver.Firefox(firefox_profile=profile)
+
+proxy.new_har("google")
+driver.get("https://google.com")
+har = proxy.har
+
+server.stop()
+driver.quit()
 
 app = Flask(__name__)
 
 url = ""
+
+
 @app.route('/', methods=["GET", "POST"])
 def get():
     if request.method == "POST":
