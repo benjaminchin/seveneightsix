225bba515a723ef53a9c746fec687d5c24eb6813
benjaminchin <benjaminchin@vt.edu>
2023-02-05 10:33:45 -0500

    cleaning up

--- src/Testing.py
+++
@@ -1,4 +0,0 @@
-import requests
-response = requests.get('https://ecofriendlyweb.org/eco-friendly-website-accreditation/')
-x = len(response.content)
-print(str(x))
--- src/req.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-r = requests.get(url="https://github.com/benjaminchin")
-print(str(len(r.content)) + " bytes")
-print(r.content)
