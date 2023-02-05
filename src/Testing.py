66ba628397d24e6d20e1da7cc8aed02e4bedef5f
JakeJewsbury <jake.jewsbury@gmail.com>
2023-02-04 19:36:08 -0500

    Not changing much

--- src/Testing.py
+++ src/Testing.py
@@ -1,7 +1,4 @@
-import pycurl
-
-url = 'https://www.reddit.com/r/webdev/comments/42g4rq/tool_to_check_how_much_data_is_needed_to_fully/'
-c = pycurl.Curl()
-c.setopt(c.URL, url)
-c.perform()
-print (c.getinfo(c.CONTENT_LENGTH_DOWNLOAD))
+import requests
+response = requests.get('https://ecofriendlyweb.org/eco-friendly-website-accreditation/')
+x = len(response.content)
+print(str(x))
