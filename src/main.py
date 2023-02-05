33b44b1255a04e6c90545bea378a111fb2cf506b
benjaminchin <benjaminchin@vt.edu>
2023-02-04 20:50:46 -0500

    missing about us section

--- src/main.py
+++ src/main.py
@@ -6,13 +6,15 @@ app = Flask(__name__)
 
 url = ""
 
-
+@app.route('/home/')
 @app.route('/', methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         url = request.form.get("url")
         visitors = request.form.get("visitors")
-        return "The requested URL is: " + str(url) + "\n The emissions for the given site is: " + str(calculate(url, visitors)) + " kilograms of CO2 per month."
+        '''return "The requested URL is: " + str(url) + "\n The emissions for the given site is: " + str(calculate(url, visitors)) + " kilograms of CO2 per month."'''
+        data = calculate(url, visitors)
+        return render_template("display.html", data=data)
     return render_template("template.html")
 
 
